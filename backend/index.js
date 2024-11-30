16b33bee885d705fce20a352b5ec23a0069b8361
Dimitar Mitev <d.mitev920123@gmail.com>
2024-11-30 14:58:54 +0200

    feat: mongodb and mongoose schemas

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,21 @@ import express from "express";
 import ImageKit from "imagekit";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL }));
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const imagekit = new ImageKit({
   urlEndpoint: process.env.IMAGE_KIT_ENDPOINT,
@@ -21,5 +30,6 @@ app.get("/api/upload", (req, res) => {
 });
 
 app.listen(PORT, () => {
+  connect();
   console.log(`Listening on port ${PORT}`);
 });
