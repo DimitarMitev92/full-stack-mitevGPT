0ee5e6bb2f374d407cc7709a76b1200cf413bd61
Dimitar Mitev <d.mitev920123@gmail.com>
2024-11-30 20:53:41 +0200

    feat: connect mongodb and create schemas

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,16 @@ import ImageKit from "imagekit";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import UserChats from "./models/userChar.js";
+import Chat from "./models/chat.js";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors({ origin: process.env.CLIENT_URL }));
+
+app.use(express.json());
 const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGO);
@@ -27,6 +31,59 @@ const imagekit = new ImageKit({
 app.get("/api/upload", (req, res) => {
   const result = imagekit.getAuthenticationParameters();
   res.send(result);
+});
+
+app.post("/api/chats", async (req, res) => {
+  const { userId, text } = req.body;
+
+  try {
+    // CREATE A NEW CHAT
+    const newChat = new Chat({
+      userId,
+      history: [{ role: "user", parts: [{ text }] }],
+    });
+
+    const savedChat = await newChat.save();
+
+    // CHECK IF THE USERCHATS EXISTS
+    const userChats = await UserChats.find({ userId });
+
+    // IF DOESN'T EXIST CREATE A NEW ONE AND ADD THE CHAT IN THE CHATS ARRAY
+    if (!userChats.length) {
+      const newUserChats = new UserChats({
+        userId,
+        chats: [
+          {
+            _id: savedChat._id,
+            title: text.substring(0, 40),
+            createdAt: Date.now(),
+          },
+        ],
+      });
+
+      await newUserChats.save();
+    } else {
+      // IF EXISTS, PUSH THE CHAT TO THE EXISTING ARRAY
+      await UserChats.updateOne(
+        {
+          userId,
+        },
+        {
+          $push: {
+            chats: {
+              _id: savedChat._id,
+              title: text.substring(0, 40),
+            },
+          },
+        }
+      );
+
+      res.status(201).send(newChat._id);
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Error creating chat!");
+  }
 });
 
 app.listen(PORT, () => {
